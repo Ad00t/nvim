fe03de574c5adb6d926c63398fd7b89ecfe13104
Adhit Siripurapu <adhit@engineering.ucsb.edu>
2025-05-15 13:18:06 -0700

    fixed shell

--- init.lua
+++ init.lua
@@ -35,3 +35,8 @@ require "nvchad.autocmds"
 vim.schedule(function()
   require "mappings"
 end)
+
+vim.opt.shell = 'C:\\Program Files\\Git\\usr\\bin\\bash.exe'
+vim.opt.shellcmdflag = '-c'
+vim.opt.shellquote = ''
+vim.opt.shellxquote = ''
