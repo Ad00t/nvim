1e7230b19fa77e43e206e2e1b8e0e76b9056499c
Adhit Siripurapu <adhit.sirip@gmail.com>
2025-09-26 21:08:16 -0700

    lspconfig updates

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,18 +1,18 @@
-local lspconfig = require("lspconfig")
 local on_attach = require("nvchad.configs.lspconfig").on_attach
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local servers = { "lua_ls", "html", "cssls", "clangd", "pyright", "ts_ls" }
 
 for _, server in ipairs(servers) do
-  lspconfig[server].setup({
-    on_attach = on_attach,
-    capabilities = capabilities,
-  })
+    vim.lsp.config(server, {
+        on_attach = on_attach,
+        capabilities = capabilities,
+    })
+    vim.lsp.enable(server)
 end
 
 for _, group in ipairs(vim.fn.getcompletion("@lsp", "highlight")) do
-  vim.api.nvim_set_hl(0, group, {})
+    vim.api.nvim_set_hl(0, group, {})
 end
 
-vim.api.nvim_set_keymap('n', '<space>d', '<cmd>lua vim.diagnostic.open_float()<CR>', { noremap=true, silent=true }) 
+vim.api.nvim_set_keymap('n', '<space>d', '<cmd>lua vim.diagnostic.open_float()<CR>', { noremap=true, silent=true })
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,12 +1,5 @@
 return {
   {
-    "neovim/nvim-lspconfig",
-    config = function()
-      require "configs.lspconfig"
-    end,
-  },
-
-  {
     "williamboman/mason.nvim",
     opts = {
       ensure_installed = {
