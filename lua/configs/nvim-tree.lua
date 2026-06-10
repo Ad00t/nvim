1f304e0ee1ccdbb0440d30aab6163ebb35ce4eea
Adhit Siripurapu <adhit@muonspace.com>
2026-06-10 13:50:15 -0700

    adaptive tree

--- lua/configs/nvim-tree.lua
+++ lua/configs/nvim-tree.lua
@@ -19,6 +19,8 @@ require("nvim-tree").setup({
     sync_root_with_cwd = true,
     update_focused_file = {
         enable = true,
-        -- update_root = true,
+    },
+    view = {
+        adaptive_size = true,
     },
 })
